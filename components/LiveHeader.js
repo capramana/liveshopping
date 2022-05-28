2681ccf29a23775668812efce4f7648fc0285e85
MattAS <matthewanthony.hds@gmail.com>
2022-05-28 02:43:40 -0400

    fixed error

--- components/LiveHeader.js
+++ components/LiveHeader.js
@@ -1,48 +1,46 @@
 import React from "react";
-import {View, StyleSheet} from "react-native";
+import { View, StyleSheet } from "react-native";
 import Viewership from "../components/Viewership";
 import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
 const LiveHeader = ({ displayName }) => {
-    return (
-        <View style ={styles.container}>
-            <View>
-                <View style={styles.userCircle}/>
-                <View>
-                    <Text>{displayName}</Text>
-                    <Viewership/>
-                </View>
-            </View>
-            <View>
-                <View style={styles.liveStudio}>
-                    <Text>LIVE</Text>
-                </View>
-                <image 
-                    source={require("../icons/close-outline.svg")}
-                />
-            </View>
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.userCircle} />
+        <View>
+          <Text>{displayName}</Text>
+          <Viewership />
         </View>
-    )
-}
+      </View>
+      <View>
+        <View style={styles.liveStudio}>
+          <Text>LIVE</Text>
+        </View>
+        <Image source={require("../icons/close-outline.svg")} />
+      </View>
+    </View>
+  );
+};
 
-const style = StyleSheet.create ({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    userCircle: {
-        height: hp(3),
-        width: hp(3),
-        borderRadius: 100,
-    },
-    liveStudio: {
-        height: hp(3),
-        width: hp(5),
-    },
-})
+const style = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userCircle: {
+    height: hp(3),
+    width: hp(3),
+    borderRadius: 100,
+  },
+  liveStudio: {
+    height: hp(3),
+    width: hp(5),
+  },
+});
 
-export default LiveHeader
+export default LiveHeader;
